10fbae17644aebb599da3c690bc2789ab63f3c70
jacksonmongbam123 <85247339+jacksonmongbam123@users.noreply.github.com>
2026-05-26 17:15:42 +0530

    Update app.js remove strict

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ const options = {
 };
 const day = today.toLocaleDateString("en-US", options);
 
-mongoose.set('strictQuery', true);
-
 mongoose.connect("mongodb+srv://jacksonmongbam123:Bpheonix@cluster0.x4dp5yy.mongodb.net/?appName=Cluster0", {useNewUrlParser: true});
 
 const itemSchema = {
